6acb23dc1c2081f07ace25665738856101a6610f
Lisowez <lisowez@icloud.com>
2024-05-07 21:43:30 +0300

    refactor: refactor app.ts file

--- src/pages/app.ts
+++ src/pages/app.ts
@@ -1,6 +1,5 @@
 import LoginPage from './login/login';
 import MainPage from './main/main';
-import Page from './page';
 import RegistrationPage from './registration/registration';
 
 export const enum PagesID {
@@ -14,33 +13,50 @@ export class App {
 
     static renderNewPage(id: string): void {
         App.container.innerHTML = '';
-        let page: null | Page = null;
-        if (id === PagesID.loginPage) {
-            page = new LoginPage(id);
-            window.location.hash = PagesID.loginPage;
-        } else if (id === PagesID.registrationPage) {
-            page = new RegistrationPage(id);
-            window.location.hash = PagesID.registrationPage;
-        } else if (id === PagesID.mainPage) {
-            page = new MainPage(id);
-            window.location.hash = PagesID.mainPage;
+
+        switch (id) {
+            case PagesID.loginPage:
+                this.renderLoginPage();
+                break;
+            case PagesID.registrationPage:
+                this.renderRegistrationPage();
+                break;
+            case PagesID.mainPage:
+                this.renderMainPage();
+                break;
+            default:
+                break;
         }
-        if (page) {
-            const pageHTML: HTMLElement = page.render();
-            App.container.append(pageHTML);
-            if (page instanceof LoginPage) {
-                const loginPage = page as LoginPage;
-                loginPage.loginRun();
-                if (page instanceof RegistrationPage) {
-                    const registrationPage = page as RegistrationPage;
-                    registrationPage.registrationRun();
-                }
-                if (page instanceof MainPage) {
-                    const gamePage = page as MainPage;
-                    gamePage.mainRun();
-                }
-            }
-        }
+    }
+
+    private static renderLoginPage() {
+        const page = new LoginPage(PagesID.loginPage);
+        window.location.hash = PagesID.loginPage;
+
+        const pageHTML = page.render();
+        App.container.append(pageHTML);
+
+        page.loginRun();
+    }
+
+    private static renderRegistrationPage() {
+        const page = new RegistrationPage(PagesID.registrationPage);
+        window.location.hash = PagesID.registrationPage;
+
+        const pageHTML = page.render();
+        App.container.append(pageHTML);
+
+        page.registrationRun();
+    }
+
+    private static renderMainPage() {
+        const page = new MainPage(PagesID.mainPage);
+        window.location.hash = PagesID.mainPage;
+
+        const pageHTML = page.render();
+        App.container.append(pageHTML);
+
+        page.mainRun();
     }
 
     private routingChange(): void {
@@ -53,13 +69,7 @@ export class App {
     public run(): void {
         const hash: string = window.location.hash.slice(1);
         window.location.hash = `#${PagesID.loginPage}`;
-        if (hash === PagesID.loginPage) {
-            App.renderNewPage(PagesID.loginPage);
-        } else if (hash === PagesID.registrationPage) {
-            App.renderNewPage(PagesID.registrationPage);
-        } else if (hash === PagesID.mainPage) {
-            App.renderNewPage(PagesID.mainPage);
-        }
+        App.renderNewPage(hash);
         this.routingChange();
     }
 }
