4384370e1a5eeabc003f9980b2e3a28a3ace4c9c
onqqr <dnbyaniss@gmail.com>
2024-05-11 01:28:03 +0300

    fix: fix template on loginpage

--- src/pages/template/loginInner.ts
+++ src/pages/template/loginInner.ts
@@ -6,17 +6,22 @@ const loginInner: string = `
       <div class="title-inputs">Авторизация</div>
       <div class="email-box">
         <span class="svg-email"></span>
-        <input class="email-input" placeholder="Введите почту" type="text" autocomplete="on">
+        <input class="email-input" placeholder="hello@example.com" type="text" autocomplete="on">
+      </div>
+      <div class="errorsbox-email">
+        <label class="error-email"></label>
       </div>
       <div class="pass-box">
         <span class="svg-lock"></span>
         <input class="pass-input" placeholder="Введите пароль" type="password" autocomplete="on">
       </div>
+      <label class="show-pass-text">
+        <input type="checkbox" class="show-pass">Показать пароль
+      </label>
     </fieldset>
   </form>
-  <div class="errors">
-    <span class="svg-alert"></span>
-    <label class="error-text"></label>
+  <div class="errorsbox-pass">
+    <label class="error-pass"></label>
   </div>
   <button type="submit" class="btn-submit" disabled>Войти</button>
   <div class="text-register">У вас нет аккаунта? <span>Регистрация</span></div>
