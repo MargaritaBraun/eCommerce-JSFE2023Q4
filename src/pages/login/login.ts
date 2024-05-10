bb3b849443476532063ca8c2dded3a9888b9f2e0
onqqr <dnbyaniss@gmail.com>
2024-05-11 01:26:05 +0300

    fix: fix logic on validations

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,3 +1,4 @@
+import { isValidEmail, isValidPass } from '../../utils/functions/validation-loginpage';
 import Page from '../page';
 import loginInner from '../template/loginInner';
 import './login.scss';
@@ -8,5 +9,42 @@ export default class LoginPage extends Page {
         return this.container;
     }
 
-    public run() {}
+    // активация кнопки Войти после пройденой валидации
+    public isValidation() {
+        const btn: HTMLElement | null = document.querySelector('.btn-submit');
+        if (btn) {
+            const inputEmail: HTMLInputElement = document.querySelector('.email-input') as HTMLInputElement;
+            const inputPass: HTMLInputElement = document.querySelector('.pass-input') as HTMLInputElement;
+            const isValid = () => {
+                const isValidationEmail: boolean = isValidEmail(inputEmail);
+                const isValidationPass: boolean = isValidPass(inputPass);
+
+                btn.style.cursor = isValidationEmail && isValidationPass ? 'pointer' : 'auto';
+                btn.style.opacity = isValidationEmail && isValidationPass ? '1' : '0.5';
+            };
+
+            inputEmail.addEventListener('input', isValid);
+            inputPass.addEventListener('input', isValid);
+        }
+    }
+
+    // чек-бокс Показать пароль
+    private showPass() {
+        const btnShowPass = document.querySelectorAll('.show-pass');
+        const input: HTMLInputElement = document.querySelector('.pass-box input.pass-input') as HTMLInputElement;
+
+        const toggleType = () => {
+            if (input.type === 'password') {
+                input.type = 'text';
+            } else {
+                input.type = 'password';
+            }
+        };
+        btnShowPass.forEach((item) => item.addEventListener('click', toggleType));
+    }
+
+    public run() {
+        this.isValidation();
+        this.showPass();
+    }
 }
