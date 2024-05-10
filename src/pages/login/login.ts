01d306eff6a247ad5a0ae930af84506041c68b84
onqqr <dnbyaniss@gmail.com>
2024-05-10 13:58:20 +0300

    feat: add styles in loginpage

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,8 +1,10 @@
 import Page from '../page';
+import loginInner from '../template/loginInner';
+import './login.scss';
 
 export default class LoginPage extends Page {
     public render(): HTMLElement {
-        this.container.innerHTML = 'Login Page';
+        this.container.innerHTML = loginInner;
         return this.container;
     }
 
