95c25e58f95386eee46f46a70a6d2461b5947fe5
onqqr <dnbyaniss@gmail.com>
2024-05-18 16:19:28 +0300

    feat: add basket page

--- src/pages/basket/basket.ts
+++ src/pages/basket/basket.ts
@@ -1,2 +1,10 @@
-const nameBasketTemplate: string = 'Класс для отрисовки страницы корзины';
-export default nameBasketTemplate;
+import Page from '../page';
+
+export default class BasketPage extends Page {
+    public render(): HTMLElement {
+        this.container.innerHTML = 'Basket Page';
+        return this.container;
+    }
+
+    public run() {}
+}
